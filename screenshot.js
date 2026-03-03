96a524b94270d77ad5b3c04c25c218b002a37429
laxmitesting7 <laxmitesting7@gmail.com>
2026-03-03 15:34:06 +0530

    Create screenshot.js

---
+++ screenshot.js
@@ -0,0 +1,9 @@
+const { chromium } = require("playwright");
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:3000", { waitUntil: "networkidle" });
+  await page.screenshot({ path: "homepage.png", fullPage: true });
+  await browser.close();
+})();
